c3b446172811b99a7e08b859e67e89d6ecc84f78
冯璠 <fengfan@zhongan.io>
2018-12-03 12:02:36 +0800

    pre

--- code_re/cnn/prepro_i.py
+++ code_re/cnn/prepro_i.py
@@ -30,31 +30,95 @@ def find_entity(path):
                     continue
                 rid, r = line.rstrip().split('\t')
                 r_name, arg1, arg2 = r.split(' ')
-                e1_info = entity_dict.get(arg1.split(":")[-1])
-                e2_info = entity_dict.get(arg2.split(":")[-1])
+                if entity_dict.get(arg1.split(":")[-1])['e_name'] == r_name.split('_')[0]:
+                    e1_info = entity_dict.get(arg1.split(":")[-1])
+                    e2_info = entity_dict.get(arg2.split(":")[-1])
+                else:
+                    e1_info = entity_dict.get(arg2.split(":")[-1])
+                    e2_info = entity_dict.get(arg1.split(":")[-1])
 
                 # cut sentence
                 min_idx = min(e1_info['s_idx'], e1_info['e_idx'], e2_info['s_idx'], e2_info['e_idx'])
                 max_idx = max(e1_info['s_idx'], e1_info['e_idx'], e2_info['s_idx'], e2_info['e_idx'])
 
-                i = min_idx - 1
-                while i < min_idx:
-                    if lines[i] == "。" and lines[i - 2: i] != "  ":
-                        break
-                    i -= 1
+                # 确定子句开始位置
+                def get_s_idx(newline_cnt, idx):
+                    # 有两个\n
+                    if newline_cnt > 1:
+                        new_line_sub_cnt = 0
+                        while idx < min_idx:
+                            if lines[idx] == "\n":
+                                new_line_sub_cnt += 1
+                                # 截取至倒数第三个\n，或是已经到开头
+                                if new_line_sub_cnt > 2 or idx == 0:
+                                    break
+                            idx -= 1
+                    # 有一个\n
+                    elif newline_cnt > 0:
+                        while idx< min_idx:
+                            if lines[idx] == "\n" or idx == 0:
+                                break
+                            idx -= 1
+                    else:
+                        raise 'min_idx前50字内既没有句号，也没有换行符!! rid: {}'.format(rid)
 
-                j = max_idx
-                while j >= max_idx:
-                    if lines[j] == "。" and lines[j - 2: j] != "  ":
-                        break
-                    j += 1
+                    return idx + 1
+
+                # min_idx之前50字内没有句号
+                if "。" not in lines[0 if min_idx-50<0 else min_idx-50: min_idx]:
+                    newline_cnt = len([c for c in lines[0 if min_idx-50<0 else min_idx-50: min_idx] if c == '\n'])
+                    i = min_idx - 1
+                    # 有两个\n
+                    if newline_cnt > 1:
+                        new_line_sub_cnt = 0
+                        while i < min_idx:
+                            if lines[i] == "\n":
+                                new_line_sub_cnt += 1
+                                # 截取至倒数第三个\n，或是已经到开头
+                                if new_line_sub_cnt > 2 or i == 0:
+                                    break
+                            i -= 1
+                    # 有一个\n
+                    elif newline_cnt > 0:
+                        while i < min_idx:
+                            if lines[i] == "\n" or i == 0:
+                                break
+                            i -= 1
+                    else:
+                        raise 'min_idx前50字内既没有句号，也没有换行符!! rid: {}'.format(rid)
+
+                else:
+                    i = min_idx - 1
+                    while i < min_idx:
+                        if lines[i] == "。" and lines[i - 2: i] != "  ":
+                            break
+                        i -= 1
+
+                # 确定子句终止位置
+                if "。" not in lines[max_idx: max_idx+50]:
+                    j = max_idx
+                    newline_cnt = len([c for c in lines[max_idx: max_idx+50] if c == '\n'])
+                    if newline_cnt > 1:
+                        new_line_sub_cnt = 0
+                        while i >= max_idx:
+                            
+
+                else:
+                    j = max_idx
+                    while j >= max_idx:
+                        if lines[j] == "。" and lines[j - 2: j] != "  ":
+                            break
+                        j += 1
 
                 sub_sent = lines[i + 1: j + 1]
-                sub_sent_ent = sub_sent[:e1_info['s_idx']] + "<e1>" + \
-                               sub_sent[e1_info['s_idx']: e1_info['e_idx']] + '</e1>' + \
-                               sub_sent[e1_info['e_idx']: e2_info['s_idx']] + '<e2>' + \
-                               sub_sent[e2_info['s_idx']: e2_info['e_idx']] + '</e2>' + \
-                               sub_sent[e2_info['e_idx']:]
+                sub_sent_s_idx = i+1
+                sub_sent_e_idx = j+1
+
+                sub_sent_ent = lines[sub_sent_s_idx:e1_info['s_idx']] + "<e1>" + \
+                               lines[e1_info['s_idx']: e1_info['e_idx']] + '</e1>' + \
+                               lines[e1_info['e_idx']: e2_info['s_idx']] + '<e2>' + \
+                               lines[e2_info['s_idx']: e2_info['e_idx']] + '</e2>' + \
+                               lines[e2_info['e_idx']:sub_sent_e_idx]
                 sub_sent_ent = re.sub('\n', '', sub_sent_ent)
 
                 sample.append((r_name, sub_sent_ent, file_name.split(".")[0], rid))
